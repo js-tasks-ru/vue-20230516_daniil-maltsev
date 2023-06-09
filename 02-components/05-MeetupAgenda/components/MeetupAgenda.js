3088f3fd62a3b83bf7f19d0e71f28027b4241833
Daniil Maltsev <daniil.malcev@inbox.ru>
2023-06-09 10:37:47 -0400

    Solution MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,5 +1,5 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js'; 
 
 export default defineComponent({
   name: 'MeetupAgenda',
