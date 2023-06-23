67ab87f38b43829ad394a39ac0d122e15606d8c9
Daniil Maltsev <daniil.malcev@inbox.ru>
2023-06-23 01:32:04 -0400

    Toaster Container

---
+++ 04-vue-cli/02-TheToaster/components/UiToastContainer.vue
@@ -0,0 +1,45 @@
+<template>
+
+<TransitionGroup :tag="tag" name="fade-list" class="fade-list toasts" mode="out-in">
+    <slot ></slot>
+  </TransitionGroup>
+</template>
+
+<script>
+export default {
+  name: 'UiToastContainer',
+  props: {
+    tag: {
+      type: [String, Object, Function],
+      default: 'div',
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fade-list {
+  position: relative;
+}
+
+.fade-list :deep( > *) {
+  opacity: 1;
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-list :deep(.fade-list-leave-active)  {
+  position: absolute !important;
+  left: 0;
+  right: 0;
+}
+
+.fade-list :deep(.fade-list-enter-from) ,
+.fade-list :deep(.fade-list-leave-to) {
+  opacity: 0;
+}
+
+.fade-list :deep(.fade-list-move)  {
+  transition: transform 0.5s;
+}
+
+</style>
