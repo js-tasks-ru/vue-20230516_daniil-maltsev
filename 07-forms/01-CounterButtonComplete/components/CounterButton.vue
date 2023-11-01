4e1153c056c684788baa51ad87a4ec04b37581ec
Daniil Maltsev <daniil.malcev@inbox.ru>
2023-11-01 16:22:41 -0400

    Solution for CounterButton

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,34 @@
 <template>
-  <button></button>
+  <button @click="handleClick">{{ state }}</button>
 </template>
 
 <script>
 export default {
+  props: {
+    count : {
+      type: Number,
+      default: 0
+    }
+  },
+  emits: ['update:count'],
   name: 'CounterButton',
+  data(){
+    return{
+      state: this.count
+    }
+  },
+  methods: {
+    handleClick(){
+      this.state += 1;
+      this.$emit('update:count', this.state)
+    }
+  },
+  watch: {
+    count(newCount){
+      if(this.state != newCount) {
+        this.state = newCount
+      }
+    }
+  }
 };
 </script>
